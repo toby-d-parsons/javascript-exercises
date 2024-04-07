708a752580fb2ed9a3990bf27bca4d88ace59b78
Toby Parsons <toby.d.parsons@gmail.com>
2024-04-07 20:34:17 +0100

    Write functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((acc, nextValue) => {
+    return acc + nextValue;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acc, nextValue) => {
+    return acc * nextValue;
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let c = 1;
+  let i = 0;
+  while (i < b) {
+    c = a * c
+    i++;
+  }
+  return c;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factNum = 1;
+  for (; num > 0; num--) {
+    factNum *= num;
+  }
+  return factNum;
 };
 
 // Do not edit below this line
