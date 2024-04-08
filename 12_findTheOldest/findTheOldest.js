f1d297aba6454f175d2b1dd6b7c596d23b08da96
Toby Parsons <toby.d.parsons@gmail.com>
2024-04-08 21:04:51 +0100

    final exercise completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,11 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+    let oldy = people.reduce((acc, nextItem) => {
+        let accAge = acc.yearOfDeath ? acc.yearOfDeath - acc.yearOfBirth : 2024 - acc.yearOfBirth;
+        let nextAge = nextItem.yearOfDeath ? nextItem.yearOfDeath - nextItem.yearOfBirth : 2024 - nextItem.yearOfBirth;
+        return accAge >= nextAge ? acc : nextItem;
+    }, people[0]);
+    return oldy;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
