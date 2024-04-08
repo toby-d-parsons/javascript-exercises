fe67b89d3fb3b313babb236c8147972c8c8d4965
Toby Parsons <toby.d.parsons@gmail.com>
2024-04-08 16:32:59 +0100

    complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(pos) {
+    if (pos < 0 ) {
+        return "OOPS";
+    } else {
+        let arr = [0, 1];
+        for (i = 0; i <= pos; i++) {
+            let fibNum = arr[i] + arr[i+1];
+            arr.push(fibNum);
+        }
+        return arr[pos];    
+    }
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
