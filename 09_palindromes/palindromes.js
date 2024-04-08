40f1215f6a26a4588620284281c6fbda1e28751c
Toby Parsons <toby.d.parsons@gmail.com>
2024-04-08 15:34:20 +0100

    resolved palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let forwardArray = string
+        .toLowerCase()
+        .split("")
+        .filter((item) => (/[a-zA-Z]/.test(item) || /^-?\d+$/.test(item)));
+    let backwardArray = forwardArray.slice().reverse();
+    return (forwardArray.join("") === backwardArray.join(""));
 };
 
 // Do not edit below this line
