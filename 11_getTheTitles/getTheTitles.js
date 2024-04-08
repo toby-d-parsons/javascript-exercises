42017feadd45b6f85b3bafa1a7a5dbbf13eedc52
Toby Parsons <toby.d.parsons@gmail.com>
2024-04-08 16:42:15 +0100

    reduce exercise complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(item) {
+    return item.reduce((acc, currentValue) => {
+        acc.push(currentValue.title);
+        return acc;
+    }, [])
 };
 
 // Do not edit below this line
